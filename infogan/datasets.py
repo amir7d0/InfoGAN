cf332ba135b610ae33df82af43b65529e91130e3
amir7d0 <amirahmadi7d0@gmail.com>
2023-02-27 17:24:50 +0330

    Add fashion_mnist dataset

--- infogan/datasets.py
+++ infogan/datasets.py
@@ -1,9 +1,15 @@
 import tensorflow as tf
-from infogan.config import config
 
-class MnistDataset(object):
-    def __init__(self):
-        (train_images, train_labels), (test_images, test_labels) = tf.keras.datasets.mnist.load_data()
+def get_dataset(dataset_name, batch_size)
+    if dataset_name == 'mnist':
+        return MNIST(batch_size)
+    if dataset_name == 'fashion_mnist':
+        return FashionMNIST(batch_size)
+
+class MNIST(object):
+    def __init__(self, batch_size):
+        self.batch_size = batch_size
+        (train_images, _), (_, _) = tf.keras.datasets.mnist.load_data()
         print(f"Number of examples: {len(train_images)}")
         print(f"Shape of the images in the dataset: {train_images.shape[1:]}")
         self.image_shape = (*train_images.shape[1:], 1)
@@ -12,12 +18,30 @@ class MnistDataset(object):
         train_images = (train_images - 127.5) / 127.5
 
         ds = tf.data.Dataset.from_tensor_slices(train_images)
-        ds = ds.shuffle(30000).batch(config.batch_size)
+        ds = ds.shuffle(30000).batch(self.batch_size)
         self.dataset = ds
     def get_dataset(self):
         return self.dataset
 
 
-class MyDataset(object):
-    def __init__(self):
+class FashionMNIST(object):
+    def __init__(self, batch_size):
+        self.batch_size = batch_size
+        (train_images, _), (_, _) = tf.keras.datasets.fashion_mnist.load_data()
+        print(f"Number of examples: {len(train_images)}")
+        print(f"Shape of the images in the dataset: {train_images.shape[1:]}")
+        self.image_shape = (*train_images.shape[1:], 1)
+        # Reshape each sample to (28, 28, 1) and normalize the pixel values in the [-1, 1] range
+        train_images = train_images.reshape(train_images.shape[0], *self.image_shape).astype("float32")
+        train_images = (train_images - 127.5) / 127.5
+
+        ds = tf.data.Dataset.from_tensor_slices(train_images)
+        ds = ds.shuffle(30000).batch(self.batch_size)
+        self.dataset = ds
+    def get_dataset(self):
+        return self.dataset
+
+
+class SVHN:
+    def __init__(self, batch_size):
         pass
