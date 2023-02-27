90a7d6555f19fdfa0dcb9b931b358954144aac0a
amir7d0 <amirahmadi7d0@gmail.com>
2023-02-27 17:24:33 +0330

    Add dataset_name

--- infogan/config.py
+++ infogan/config.py
@@ -6,7 +6,7 @@ class Config:
         self.gen_lr = 1e-3
         self.dis_lr = 2e-4
 
-        self.name = 'mnist'
+        self.dataset_name = 'mnist' ### choose dataset from ['mnist', 'fashion_mnist', 'svhn', 'celeb_a']
         self.root_log_dir = "logs/"
         self.root_checkpoint_dir = "ckt/"
 
