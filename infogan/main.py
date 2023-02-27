d3b16a36f4a8495ed76f03d0eff84d23158eb1d6
amir7d0 <amirahmadi7d0@gmail.com>
2023-02-27 17:27:14 +0330

    Add lambda, and get_dataset

--- infogan/main.py
+++ infogan/main.py
@@ -3,7 +3,7 @@ import tensorflow as tf
 import os
 
 from infogan.models import get_discriminator_model, get_generator_model, get_recognition_model
-from infogan.datasets import MnistDataset
+from infogan.datasets import get_dataset
 from infogan.infogan_model import InfoGAN
 from infogan.config import config
 from infogan.distributions import Uniform, Categorical
@@ -12,19 +12,18 @@ from infogan.funcs import InfoGANMonitor, InfoGANCheckpoint
 tf.config.run_functions_eagerly(True)
 
 if __name__ == "__main__":
-    exp_name = f"{config.name}"
-    log_dir = os.path.join(config.root_log_dir, exp_name)
-    checkpoint_dir = os.path.join(config.root_checkpoint_dir, exp_name)
+    log_dir = os.path.join(config.root_log_dir, config.dataset_name)
+    checkpoint_dir = os.path.join(config.root_checkpoint_dir, config.dataset_name)
     os.makedirs(log_dir, exist_ok=True)
     os.makedirs(checkpoint_dir, exist_ok=True)
 
-    ### load dataset
-    mnist_dataset = MnistDataset()
+    # load dataset
+    mnist_dataset = get_dataset(config.dataset_name, config.batch_size)
     dataset = mnist_dataset.get_dataset()
 
-    ### define latent codes specification
+    # define latent codes specification
     latent_spec = {
-        'noise-variables': [Uniform(62)],
+        'noise-variables': [Uniform(62)],    # for `noise-variables` just specify one dist in list format
         'continuous-latent-codes': [Uniform(1), Uniform(1)],
         'discrete-latent-codes': [Categorical(10)]
     }
@@ -35,16 +34,15 @@ if __name__ == "__main__":
 
     info_gan = InfoGAN(
         generator=generator, discriminator=discriminator, recognition=recognition,
-        latent_spec=latent_spec
-    )
+        latent_spec=latent_spec, discrete_reg_coeff=1.0, continuous_reg_coeff=1.0)
 
-    ### Restore the latest checkpoint
+    # Restore the latest checkpoint
     # checkpoint = tf.train.Checkpoint(info_gan)
     # checkpoint.restore(tf.train.latest_checkpoint(checkpoint_dir))
 
     info_gan.compile(
-        g_optimizer= tf.keras.optimizers.Adam(1e-3, 0.5),
-        d_optimizer= tf.keras.optimizers.Adam(2e-4, 0.5),
+        g_optimizer=tf.keras.optimizers.Adam(1e-3, 0.5),
+        d_optimizer=tf.keras.optimizers.Adam(2e-4, 0.5),
         loss_fn=tf.keras.losses.BinaryCrossentropy(from_logits=True)
     )
     monitor_cbk = InfoGANMonitor(latent_spec=latent_spec, log_dir=log_dir)
