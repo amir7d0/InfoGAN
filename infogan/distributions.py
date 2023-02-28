a0107782948f4e6a5c0088369271fba1f5ad51f5
amir7d0 <amirahmadi7d0@gmail.com>
2023-02-28 23:37:21 +0330

    Add GaussianNLLLoss, LogProb

--- infogan/distributions.py
+++ infogan/distributions.py
@@ -3,6 +3,9 @@ import tensorflow as tf
 import tensorflow_probability as tfp
 tfd = tfp.distributions
 
+TINY = 1e-6
+
+
 class Categorical(object):
     def __init__(self, dim):
         self.dim = dim
@@ -19,6 +22,7 @@ class Categorical(object):
         z_cat = tf.one_hot(z_cat, self.dim)
         return z_cat
 
+
 class Uniform(object):
     def __init__(self, dim):
         self.dim = dim
@@ -29,4 +33,27 @@ class Uniform(object):
     def sample_test(self, c, batch_size):
         # for sampling specific number, e.g. c=0.5
         return np.array([c] * batch_size).reshape([batch_size, 1])
+
+
+class GaussianNLLLoss:
+    def __call__(self, x, z_mean, z_log_var):
+        r"""Gaussian negative log likelihood loss.
+
+        Compute negative log-likelihood for Gaussian distribution
+        L = 0.5 * [log(2 * pi * var) + ((x - mu)**2)/var]
+        mu: the mean (μ), var: the variance (σ^2)
+        """
+        mu, var = z_mean, tf.math.exp(z_log_var)
+        loss = 0.5 * (tf.math.log(2 * np.pi * var + TINY) + (x - mu) ** 2 / (var + TINY))
+        return tf.reduce_mean(loss)
+
+
+class LogProb:
+    def __call__(self, x, z_mean, z_log_var):
+        r"""
+        Returns the log of the probability density/mass function evaluated at value.
+        """
+        mu, var = z_mean, tf.math.exp(z_log_var)
+        neg_l = -0.5 * (tf.math.log(2 * np.pi * var + TINY) + (x - mu) ** 2 / (var + TINY))
+        return tf.reduce_sum(neg_l, axis=1)
 
