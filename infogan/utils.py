34f842c589ddce695e92325704ba051dab30aa2e
amir7d0 <amirahmadi7d0@gmail.com>
2023-03-01 19:43:55 +0330

    Add n_row, n_col to plot func

--- infogan/utils.py
+++ infogan/utils.py
@@ -32,27 +32,29 @@ def sample_test(latent_spec, batch_size,
 
 
 def plot_test(generator, latent_spec,
-              idx_range_varying_disc=(0, [0, 5]),
+              n_row=10, n_col=10,
+              idx_range_varying_disc=(0, [0, 10]),
               idx_range_varying_cont=(0, [-2, 2])):
 
     disc_idx, disc_range = idx_range_varying_disc
     cont_idx, cont_range = idx_range_varying_cont
-    step = (cont_range[1] - cont_range[0]) / 10    # 10 is the number of columns
-    output_image = []
-    for cat in np.arange(disc_range[0], disc_range[1], 1):
+
+    cat_codes = np.random.choice(np.arange(disc_range[0], disc_range[1], 1), size=n_row, replace=False)
+    output_images = []
+    for cat in cat_codes:
         var_cont_images = []
-        for cont in np.arange(cont_range[0], cont_range[1], step):
+        for cont in np.linspace(cont_range[0], cont_range[1], n_col):
             noise, _, cc = sample_test(latent_spec, batch_size=1,
                                        discrete_idx_c=(disc_idx, cat),
                                        continuous_idx_c=(cont_idx, cont))
-            imgs = generator(noise)
-            imgs = (imgs.numpy() + 1.) / 2.
+            imgs = generator(noise, training=False)
+            imgs = (imgs.numpy() * 127.5) + 127.5
             var_cont_images.append(imgs.reshape([28, 28]))
-        output_image.append(np.concatenate(var_cont_images, 1))
+        output_images.append(np.concatenate(var_cont_images, 1))
 
-    output_image = np.concatenate(output_image, 0)
+    output_image = np.concatenate(output_images, 0)
     plt.figure(figsize=(20, 10))
-    plt.title(f"varying discrete latent code {disc_idx}, varying continuous latent code {cont_idx}")
+    plt.title(f"varying discrete latent code {disc_idx} in [{disc_range[0]}, {disc_range[1]}], varying continuous latent code {cont_idx} in [{cont_range[0]}, {cont_range[1]}]")
     plt.imshow(output_image, cmap="gray")
     plt.axis("off")
     plt.savefig(f'varying-discrete-{disc_idx}_varying-continuous-{cont_idx}')
@@ -75,7 +77,7 @@ class InfoGANMonitor(tf.keras.callbacks.Callback):
         fig = plt.figure(figsize=(self.n_row, self.n_row))
         for i in range(predictions.shape[0]):
             plt.subplot(self.n_row, self.n_row, i+1)
-            plt.imshow(predictions[i, :, :] * 127.5 + 127.5, cmap='gray')
+            plt.imshow(predictions[i, :, :], cmap='gray')
             plt.axis('off')
         fig.savefig(f'{self.log_dir}/img_epoch_{epoch+1:04d}.png')
         plt.close(fig)
