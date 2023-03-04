d23616852971b3be14708fb94a07407b9c3880ef
amir7d0 <amirahmadi7d0@gmail.com>
2023-03-04 20:36:14 +0330

    Add lambda to config

--- infogan/config.py
+++ infogan/config.py
@@ -1,10 +1,12 @@
 
 class Config(object):
     def __init__(self):
-        self.epochs = 10
+        self.epochs = 50
         self.batch_size = 128
         self.gen_lr = 1e-3
         self.dis_lr = 2e-4
+        self.lambda_disc = 1.0
+        self.lambda_cont = 0.1
 
         self.dataset_name = 'mnist'  # choose dataset from ['mnist', 'fashion_mnist', 'svhn', 'celeb_a']
         self.root_log_dir = "logs/"
@@ -14,3 +16,4 @@ class Config(object):
 
 
 config = Config()
+
